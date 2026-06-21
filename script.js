020f8749a37643457f5d040988806c6e764686b3
afni1011 <afniana17@gmail.com>
2026-06-21 21:42:41 +0700

    menambahkan fungsi tombol mulai gratis sebelum react

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnMulaiGratis = document.querySelector('#startButton');
+const tagInfo = document.querySelector('#tag-info');
+
+let counter = 0;
+
+btnMulaiGratis.addEventListener('click', () => {
+  counter++;
+
+  if (counter === 1) {
+    console.log('clicked');
+
+    tagInfo.classList.remove('hidden');
+    tagInfo.textContent = 'Clicked! Tombol Mulai Gratis berhasil diklik.';
+  } else if (counter === 2) {
+    console.log('off');
+
+    tagInfo.textContent = 'Off! Tombol sudah diklik dua kali.';
+    btnMulaiGratis.classList.add('hidden');
+  }
+});
